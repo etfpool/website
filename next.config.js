263dcee55ec90ed7ce51f8011d92498f6ab26a3c
hungdoba <hungdoba.hdb@gmail.com>
2022-11-13 21:33:11 +0900

    config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: 'Rgeen',
     API_DEVELOPMENT: 'http://localhost:3000/api',
-    API_PRODUCTIOM: 'https://rgeen.com/api',
+    API_PRODUCTIOM: 'https://www.rgeen.com/api',
     PRODECTION: false,
     DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-    DOMAIN_PRODUCTION: 'https://rgeen.com',
+    DOMAIN_PRODUCTION: 'https://www.rgeen.com',
     GOOGLE_CLIENT_ID:
       '608822348611-eacd6jb18qc5e9v5afb3h3iuqcpfbadj.apps.googleusercontent.com',
   },
